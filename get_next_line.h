847e46a446d93749ab846f5c1a0c35dd4224368a
Maelle PAINCHAULT <mpaincha@e2r4p15.42.fr>
2016-01-22 10:19:55 +0100

    correction de la libft

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mpaincha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/03 11:34:46 by mpaincha          #+#    #+#             */
-/*   Updated: 2016/01/08 15:48:15 by mpaincha         ###   ########.fr       */
+/*   Updated: 2016/01/18 15:48:40 by mpaincha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft.h"
+# include "libft/includes/libft.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mpaincha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/03 13:38:36 by mpaincha          #+#    #+#             */
-/*   Updated: 2016/01/03 13:38:40 by mpaincha         ###   ########.fr       */
+/*   Updated: 2016/01/22 10:03:07 by mpaincha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,18 @@ int		main(int argc, char const *argv[])
 	int		fd;
 	char	*line;
 
-	if (argc == 4)
+	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd == -1 || fd1 == -1 || fd2 == -1)
+		if (fd == -1)
 			return (-1);
-		while (get_next_line(fd, &line))
+		while ((get_next_line(fd, &line)))
 		{
 			ft_putstr(line);
 			free(line);
 			ft_putstr("\n");
 		}
 	}
+	while (1);
 	return (0);
 }
